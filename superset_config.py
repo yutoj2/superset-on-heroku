23004e0b10219ed100009d92dba14549d0649b7f
Gil Raphaelli <g@raphaelli.com>
2017-06-20 16:49:54 -0400

    more env vars

--- superset_config.py
+++ superset_config.py
@@ -4,15 +4,15 @@ import os
 #---------------------------------------------------------
 # ROW_LIMIT = 5000
 SUPERSET_WORKERS = 1 # for it to work in heroku basic/hobby dynos increase as you like
-
 SUPERSET_WEBSERVER_PORT = os.environ['PORT']
 #---------------------------------------------------------
+MAPBOX_API_KEY = os.getenv('MAPBOX_API_KEY')
 
 #---------------------------------------------------------
 # Flask App Builder configuration
 #---------------------------------------------------------
 # Your App secret key
-SECRET_KEY = '\2\1thisismyscretkey\1\2\e\y\y\h'  # noqa
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # The SQLAlchemy connection string to your database backend
 # This connection defines the path to the database that stores your
